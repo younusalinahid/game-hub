a2edafe54fca81d5ff3c27f849fa6460841b207b
nahid <y.nahid@avanto-vr.com>
2025-06-24 17:21:04 +0600

    Filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,11 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
   name: string;
   slug: string;
+  metacritic: number;
 }
 
 export interface Game {
@@ -14,6 +16,9 @@ export interface Game {
   metaritic: number;
 }
 
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 
 export default useGames;
